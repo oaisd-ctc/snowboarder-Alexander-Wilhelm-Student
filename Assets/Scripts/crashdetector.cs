71f891ba18f6d2c89930fb2f144e9d83cc8eaf3a
Alexander-Wilhelm-Student <alwid46@student.oaisd.org>
2023-03-15 13:26:01 -0400

    gamingage

--- Assets/Scripts/crashdetector.cs
+++ Assets/Scripts/crashdetector.cs
@@ -14,8 +14,8 @@ public class crashdetector : MonoBehaviour
     bool dying = false;
 
     private void OnTriggerEnter2D(Collider2D other) { //instead of using just the other object's tag, we specify a collider on the gameobject to get more precise information about the collision
-        if (!dying && crashTrigger.IsTouchingLayers(crashLayer)) { // this also lets us specify exactly which trigger should do the collid
-            Debug.Log("dumbass");
+        if (!dying && crashTrigger.IsTouchingLayers(crashLayer)) { // this also lets us specify exactly which trigger should do the colliding
+            //Debug.Log("dumbass");
             
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
